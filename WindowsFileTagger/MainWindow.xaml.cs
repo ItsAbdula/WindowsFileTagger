6f06f524329c430b4222bb8dcb109ea206f81602
itsabdula <abdula2523@ajou.ac.kr>
2021-06-28 04:50:26 +0900

    기기의 폴더 이름들 TreeView로 볼 수 있는 기능 추가.

--- WindowsFileTagger/MainWindow.xaml.cs
+++ WindowsFileTagger/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,55 @@ namespace WindowsFileTagger
     /// </summary>
     public partial class MainWindow : Window
     {
+        private TreeViewItem dummyNode = null;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MakeDirectories();
+        }
+
+        private void MakeDirectories()
+        {
+            foreach (var driver in Directory.GetLogicalDrives())
+            {
+                var item = new TreeViewItem();
+                item.Header = driver;
+                item.Tag = driver;
+                item.Expanded += new RoutedEventHandler(folderExpanded);
+
+                // Expanded가능하다는 아이콘 보여주기 위해, 더미 아이템 추가.
+                // TODO: 폴더에 파일이 하나라도 있을때, 아이콘 보여주기.
+                item.Items.Add(dummyNode);
+
+                FileExplorer.Items.Add(item);
+            }
+        }
+
+        private void folderExpanded(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem)sender;
+
+            if (item.Items.Count == 1 && item.Items[0] == null)
+            {
+                item.Items.Clear();
+
+                foreach (string folder in Directory.GetDirectories(item.Tag.ToString()))
+                {
+                    var subitem = new TreeViewItem();
+                    subitem.Header = folder.Substring(folder.LastIndexOf("\\") + 1);
+                    subitem.Tag = folder;
+                    subitem.Items.Add(dummyNode);
+
+                    subitem.Expanded += new RoutedEventHandler(folderExpanded);
+
+                    item.Items.Add(subitem);
+                }
+            }
         }
     }
 }
